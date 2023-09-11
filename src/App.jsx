a19a419a316102814d9b4b4897942462863897c9
SOUGATA DAS <crusaderbeastincarnate@gmail.com>
2023-09-11 20:49:25 +0530

    Add another item to default list.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     { id: 1, description: "Passports", quantity: 2, packed: false },
     { id: 2, description: "Socks", quantity: 12, packed: false },
     { id: 3, description: "Charger", quantity: 1, packed: false },
+    { id: 4, description: "Sunglasses", quantity: 2, packed: false },
   ]);
 
   function handleAddItems(item) {
